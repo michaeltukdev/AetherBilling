1a1c3f7275f7e49415b73b0a7ccaf44652efbd65
Michael T. <66504185+michaeltukdev@users.noreply.github.com>
2024-08-05 04:38:30 +0100

    Tailwind changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
     @vite('resources/css/app.css')
     @inertiaHead
   </head>
-  <body>
+  <body class="bg-background">
     @inertia
   </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./resources/**/*.blade.php",
-    "./resources/**/*.js",
+    "./resources/**/*.jsx",
     "./resources/**/*.vue",
   ],
   theme: {
