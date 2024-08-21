7bf442cf1c211a426c8e6eff0052b54ee46333bd
Michael T. <66504185+michaeltukdev@users.noreply.github.com>
2024-08-21 21:02:04 +0100

    update: Centre  flash messages
    For some reason they were not correctly centred so that that has been fixed.
    
    I  have also limited the size of them.

--- resources/js/Components/FlashMessage.jsx
+++ resources/js/Components/FlashMessage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { FaCheckCircle, FaExclamationCircle, FaInfoCircle, FaTimes } from 'react-icons/fa';
+import { FaCheckCircle, FaExclamationCircle, FaTimes } from 'react-icons/fa';
 
 const FlashMessage = ({ message, type, duration = 4000, onClose }) => {
   const [isVisible, setIsVisible] = useState(true);
@@ -20,8 +20,8 @@ const FlashMessage = ({ message, type, duration = 4000, onClose }) => {
   return (
     <AnimatePresence>
       {isVisible && message && (
-        <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 50 }} transition={{ duration: 0.3 }} className="fixed bottom-8 left-1/2 transform -translate-x-1/2 z-50" >
-          <div  className={`px-6 py-3 rounded-lg flex items-center shadow-lg border ${ type === "error" ? "bg-red-100 text-red-800 border-red-300" : "bg-green-100 text-green-800 border-green-300" }`}  role="alert" >
+        <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 50 }} transition={{ duration: 0.3 }} className="fixed bottom-8 transform left-0 right-0 max-w-[700px] mx-auto z-50" >
+          <div  className={`px-6 py-3 rounded-lg flex items-center shadow-lg border ${ type === "error" ? "bg-red-100 text-red-700 border-red-200" : "bg-green-100 text-green-800 border-green-300" }`}  role="alert" >
             
             { type === "error" ? <FaExclamationCircle className="w-5 h-5 text-red-500" /> : <FaCheckCircle className="w-5 h-5 text-green-500" /> }
             <span className="ml-3 block text-sm font-medium">{message}</span>
