e4fd99d46a428aa9c50d35dbecfafa64c8cb7a2c
Michael T. <66504185+michaeltukdev@users.noreply.github.com>
2024-08-22 18:18:00 +0100

    fix: Fixing height issue with "MainInput"
    - Solved an issue where the line height was messing with the height and padding of the input.
    
    - Simply reduced the 'leading' (line-height) to none on all inputs by default.

--- resources/js/Components/ui/inputs/MainInput.jsx
+++ resources/js/Components/ui/inputs/MainInput.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function MainInput({ label, required = false, type = "text", value, onChange, error, placeholder = "", className = "" }) {
-  const inputClasses = `mt-3.5 bg-border rounded-lg text-text-medium text-xs border font-normal py-3 px-3.5 outline-none ${className} ${error ? 'border-red-300' : 'border-input-border'}`;
+  const inputClasses = `mt-3.5 bg-border rounded-lg text-text-medium text-xs border font-normal py-3 leading-none px-3.5 outline-none ${className} ${error ? 'border-red-300' : 'border-input-border'}`;
 
   return (
     <div>
