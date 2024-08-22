a4cc3fc6553e5178b7bc2569c722187528032512
Michael T. <66504185+michaeltukdev@users.noreply.github.com>
2024-08-22 18:16:24 +0100

    fix: Align 'close' icon in flash messages
    - Positioning of the close icon corrected to ensure proper alignment
    - Fixed the error where all text and icons were on the left and not taking the full width of the div.

--- resources/js/Components/FlashMessage.jsx
+++ resources/js/Components/FlashMessage.jsx
@@ -21,13 +21,17 @@ const FlashMessage = ({ message, type, duration = 4000, onClose }) => {
     <AnimatePresence>
       {isVisible && message && (
         <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 50 }} transition={{ duration: 0.3 }} className="fixed bottom-8 transform left-0 right-0 max-w-[700px] mx-auto z-50" >
-          <div  className={`px-6 py-3 rounded-lg flex items-center shadow-lg border ${ type === "error" ? "bg-red-100 text-red-700 border-red-200" : "bg-green-100 text-green-800 border-green-300" }`}  role="alert" >
-            
-            { type === "error" ? <FaExclamationCircle className="w-5 h-5 text-red-500" /> : <FaCheckCircle className="w-5 h-5 text-green-500" /> }
-            <span className="ml-3 block text-sm font-medium">{message}</span>
+          <div className={`px-6 py-3 rounded-lg flex justify-between items-center shadow-lg border ${type === "error" ? "bg-red-100 text-red-700 border-red-200" : "bg-green-100 text-green-800 border-green-300"}`} role="alert" >
+
+            <div className='flex items-center'>
+              {type === "error" ? <FaExclamationCircle className="w-5 h-5 text-red-500" /> : <FaCheckCircle className="w-5 h-5 text-green-500" />}
+              <span className="ml-3 block text-sm font-medium">{message}</span>
+            </div>
+
             <button onClick={() => { setIsVisible(false); onClose(); }} className="ml-6 focus:outline-none">
-              <FaTimes className="w-5 h-5 text-gray-500 hover:text-gray-700" />
+              <FaTimes className="w-4 h-4 text-background hover:text-input-border transition" />
             </button>
+
           </div>
         </motion.div>
       )}
